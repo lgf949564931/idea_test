9e7739738d5753f91b4d4155e0ade18319ed5794
lgf949564931 <949564931@qq.com>
2020-03-08 18:06:45 +0800

    第一次提交

--- manager-web/src/main/java/com/lgf/web/controller/UserController.java
+++ manager-web/src/main/java/com/lgf/web/controller/UserController.java
@@ -4,7 +4,9 @@ import com.lgf.pojo.Users;
 import com.lgf.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @Auther: lgf
@@ -21,5 +23,29 @@ public class UserController {
     public String addUser(Users users){
         this.usersService.addUser(users);
         return "ok";
+    }
+    @RequestMapping(value="/doGet/{userid}",method= RequestMethod.GET)
+    public String doGet(@PathVariable Integer userid){
+    System.out.println("doGet.........."+userid);
+    return "ok";
+    }
+    @RequestMapping(value="/doPost",method = RequestMethod.POST)
+    public String doPost(Users users){
+        System.out.println("doPost..........");
+        System.out.println(users);
+        return "ok";
+
+    }
+    @RequestMapping(value="/doDelete",method = RequestMethod.DELETE)
+    public String doDelete(Integer userid){
+        System.out.println("doDelete.........."+userid);
+        return "ok";
+
+    }
+    @RequestMapping(value="/doPut",method = RequestMethod.PUT)
+    public String doPut(){
+        System.out.println("doPut..........");
+        return "ok";
+
     }
 }
